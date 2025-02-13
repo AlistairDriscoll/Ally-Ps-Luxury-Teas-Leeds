c7cf853d8185dcad2fe9f46825ff555877466674
Alistair Driscoll <pallythecoder@gmail.com>
2025-02-13 10:04:23 +0000

    Finish bag contents context

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,7 @@
+from django.shortcuts import get_object_or_404, redirect
+from shop.models import Product
+
+
 def bag_contents(request):
     """
     For storing bag contents
@@ -5,10 +9,63 @@ def bag_contents(request):
     is registered in settings
     """
 
-    bag_items = 0
+    bag = request.session.get("bag", {})
+    sample_product_id = request.session.get("sample_product_id")
+    sample_added = bool(sample_product_id)
+
+    bag_items = []
+    total = 0
+    total_items = 0
+
+    for item_id, weights in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+
+        for weight, quantity in weights.items():
+            weight = int(weight)
+            total_items += quantity
+            if weight == 30:
+                price = product.base_price_number
+            elif weight == 100:
+                price = product.base_price_number * 3
+            elif weight == 300:
+                price = product.base_price_number * 8
+            else:
+                return redirect('product_detail', sku=product.sku)
+
+            total += price * quantity
+            bag_items.append(
+                {
+                    "product": product,
+                    "weight": weight,
+                    "quantity": quantity,
+                    "price": round(price, 2),
+                    "subtotal": round(price * quantity, 2),
+                }
+            )
+
+        total = round(total, 2)
+
+    if sample_added:
+        sample_product = get_object_or_404(Product, id=sample_product_id)
+
+        bag_items.append(
+            {
+                "product": sample_product,
+                "weight": 5,
+                "quantity": 1,
+                "price": 0,
+                "subtotal": 0,
+            }
+        )
+
+        total_items += 1
 
     context = {
         "bag_items": bag_items,
+        "total_items": total_items,
+        "total": total,
+        "sample_added": sample_added,
+        "sample_product": sample_product if sample_added else None,
     }
 
     return context
