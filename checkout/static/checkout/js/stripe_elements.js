2d44ba021a6f27228d6583d7465d1bf5b1a32eaa
Alistair Driscoll <pallythecoder@gmail.com>
2025-02-20 07:02:28 +0000

    Add: Payment HTML, url and Stripe element JS and CSS

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,35 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS from here:
+    https://stripe.com/docs/stripe-js
+*/
+
+/*
+    Finds the public key and client secret, slices off the quotation marks
+    Sets up stripe and its elements by passing through the public key
+    Creates up a card element and gives it some styling
+    Mounts the card element onto the div with the id of 'card-element'
+*/
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', { style: style });
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -16,7 +16,19 @@ def checkout(request):
     order_form = OrderForm
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form,
+        "order_form": order_form,
     }
 
     return render(request, template, context)
+
+
+def payment(request):
+    """Renders the payment section of the form"""
+
+    context = {
+        "stripe_public_key": """pk_test_51QPRXqGHaqkD0AzD6fypQbKMWij5eQ1RMRVys8
+        kd4Tyj9TxwAfmM1dpoiCNXQ2tCbPCbtGeIpW761yBFeW3ll1bF00CrFQHp3G""",
+        "client_secret": "Test client secret",
+    }
+
+    return render(request, 'checkout/payment.html', context)
