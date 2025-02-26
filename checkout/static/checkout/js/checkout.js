9582593a1dabc69830cf1faa828d92d10df9d52c
Alistair Driscoll <pallythecoder@gmail.com>
2025-02-26 15:13:57 +0000

    Calculate delivery and grand total js working

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -2,74 +2,84 @@
 /* global $ */
 
 
-const europeanCountries = ["AL",
-    "AD",
-    "AM",
-    "AT",
-    "AZ",
-    "BY",
-    "BE",
-    "BA",
-    "BG",
-    "HR",
-    "CY",
-    "CZ",
-    "DK",
-    "EE",
-    "FI",
-    "FR",
-    "GE",
-    "DE",
-    "GR",
-    "HU",
-    "IS",
-    "IE",
-    "IT",
-    "KZ",
-    "XK",
-    "LV",
-    "LI",
-    "LT",
-    "LU",
-    "MT",
-    "MD",
-    "MC",
-    "ME",
-    "NL",
-    "MK",
-    "NO",
-    "PL",
-    "PT",
-    "RO",
-    "RU",
-    "SM",
-    "RS",
-    "SK",
-    "SI",
-    "ES",
-    "SE",
-    "CH",
-    "TR",
-    "UA",
-    "VA",];
+const europeanCountries = ["AL", // Albania
+    "AD", // Andorra
+    "AM", // Armenia
+    "AT", // Austria
+    "AX", // Aland Islands
+    "AZ", // Azerbaijan
+    "BY", // Belarus
+    "BE", // Belgium
+    "BA", // Bosnia and Herzegovina
+    "BG", // Bulgaria
+    "HR", // Croatia
+    "CY", // Cyprus
+    "CZ", // Czechia (Czech Republic)
+    "DK", // Denmark
+    "EE", // Estonia
+    "FI", // Finland
+    "FR", // France
+    "GE", // Georgia
+    "DE", // Germany
+    "GR", // Greece
+    "HU", // Hungary
+    "IS", // Iceland
+    "IE", // Ireland
+    "IT", // Italy
+    "KZ", // Kazakhstan
+    "XK", // Kosovo
+    "LV", // Latvia
+    "LI", // Liechtenstein
+    "LT", // Lithuania
+    "LU", // Luxembourg
+    "MT", // Malta
+    "MD", // Moldova
+    "MC", // Monaco
+    "ME", // Montenegro
+    "NL", // Netherlands
+    "MK", // North Macedonia
+    "NO", // Norway
+    "PL", // Poland
+    "PT", // Portugal
+    "RO", // Romania
+    "RU", // Russia
+    "SM", // San Marino
+    "RS", // Serbia
+    "SK", // Slovakia
+    "SI", // Slovenia
+    "ES", // Spain
+    "SE", // Sweden
+    "CH", // Switzerland
+    "TR", // Turkey
+    "UA", // Ukraine
+    "VA", // Vatican City
+    ];
 let countrySelector = $('#id_country');
-let orderTotal = $('#order-total').value;
+let orderTotal = parseFloat($('#order-total').text());
 let hiddenDeliveryCost = $('#id_delivery_cost');
 let hiddenGrandTotal = $('#id_grand_total');
 let displayDeliveryCost = $('#display-delivery');
 let displayGrandTotal = $('#display-grand-total');
 
-countrySelector.addEventListener('change', function () {
-    if (countrySelector.value === "UK") {
-        displayDeliveryCost.value = 3;
-        hiddenDeliveryCost.value = 3;
-    } else if (europeanCountries.includes(countrySelector.value)) {
-        displayDeliveryCost.value = 7;
-        hiddenDeliveryCost.value = 7;
-    } else {
-        displayDeliveryCost.value = 15;
-        hiddenDeliveryCost.value = 15;
+/**
+ * Checks to see if country selector has been changed
+ * Checks the country, if it isn't in the UK or Europe delivery is £15
+ * Else if in europe £7 or UK £3
+ * Calculates grand total based on this and displays it, along with
+ * adding the values to the submit_order form
+ */
+countrySelector.on('change', function () {
+    let country = countrySelector.val();
+    let deliveryCost = 15;
+    if (country === "UK") {
+        deliveryCost = 3;
+    } else if (europeanCountries.includes(country)) {
+        deliveryCost = 7;
     }
-    hiddenGrandTotal.value = hiddenDeliveryCost.value + orderTotal;
-    displayGrandTotal.value = hiddenGrandTotal.value + orderTotal;
+
+    displayDeliveryCost.text("Delivery cost: " + deliveryCost);
+    hiddenDeliveryCost.val(deliveryCost);
+    let grandTotal = orderTotal + deliveryCost;
+    displayGrandTotal.text("Grand total: " + grandTotal);
+    hiddenGrandTotal.val(orderTotal + deliveryCost);
 });
