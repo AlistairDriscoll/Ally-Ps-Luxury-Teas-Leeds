3a15aad871c1de8ae57b205459fe2d192332345f
Alistair Driscoll <pallythecoder@gmail.com>
2025-03-06 14:29:10 +0000

    Add: Forn to about me

--- about/views.py
+++ about/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from .forms import EnquiryForm
+
 
 def about(request):
     """View that renders the about page"""
 
-    return render(request, 'about/about.html')
+    form = EnquiryForm
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'about/about.html', context)
