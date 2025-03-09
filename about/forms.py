a4c81610a023887011cc4b94b7dfcd45157e2b36
Alistair Driscoll <pallythecoder@gmail.com>
2025-03-09 12:26:12 +0000

    Add: Sitemap and robots files, aria labels to about me page

--- about/forms.py
+++ about/forms.py
@@ -3,11 +3,28 @@ from .models import Enquiry
 
 
 class EnquiryForm(forms.ModelForm):
-    """Forms for customer to be able to send an enquiry"""
+    """Form for customers to be able to send an enquiry"""
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={"aria-label": "Email Address", "required": True}
+        )
+    )
+    subject = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"aria-label": "Subject", "required": True}
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"aria-label": "Content", "required": True}
+        )
+    )
+
     class Meta:
         model = Enquiry
         fields = (
-            'email',
-            'subject',
-            'content',
+            "email",
+            "subject",
+            "content",
         )
